79efcdac0f2a9ae2c3787c134b234e47de38da43
Shuntia <128505810+shuntia@users.noreply.github.com>
2024-05-24 10:00:29 -0700

    Added solver.py, updated main.py

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import sys
 import os
 import argparse
 import parser
+import solver
 #I love os
 
-sys.stdout.write("This is the math solver for the Algebra 2 Honors class.")
-parser = argparse.ArgumentParser()
-parser.add_argument("-v", "--verbose", help="increase output verbosity",required=False)
-
-expression=input("Please enter your expression to solve: ")
-expression=parser.parse(expression)
+sys.stdout.write("This is the polynomial solver for the Algebra 2 Honors class.")
+pars = argparse.ArgumentParser()
+pars.add_argument("-v", "--verbose", help="increase output verbosity",required=False)
+args=pars.parse_args()
+expression=input("Please enter your expression/equasion to solve: ")
+parsedexpr=parser.parse(expression)
+eqtype=solver.find_type(parsedexpr)
+solution=solver.solve(parsedexpr, eqtype)
+sys.stdout.write("Solution is: "+str(solution))
