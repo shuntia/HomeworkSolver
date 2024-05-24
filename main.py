f4bb821ca61e953bbafa20eb20bee88b28a5302b
Shuntia <128505810+shuntia@users.noreply.github.com>
2024-05-24 09:47:09 -0700

    added stuff... IDK if it's working

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import sys
 import os
 import argparse
+import parser.py
 #I love os
 
 sys.stdout.write("This is the math solver for the Algebra 2 Honors class.")
 parser = argparse.ArgumentParser()
-parser.add_argument("-v", "--verbose", help="increase output verbosity",required=false)
+parser.add_argument("-v", "--verbose", help="increase output verbosity",required=False)
+
+expression=input("Please enter your expression to solve: ")
+expression=parser.parse(expression)
